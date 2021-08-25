d269ca007f02db755b2ccad31a377870357dd852
Felipe Umpierre <feumpi@gmail.com>
2021-08-25 10:16:45 -0300

    primeiro commit

---
+++ USO.md
@@ -0,0 +1,13 @@
+### Universidade Federal do Espírito Santo
+
+### INF09324 Programação Aplicada de Computadores 2021/1
+
+### Primeiro trabalho
+
+### Aluno: 2020205193 Felipe Pereira Umpierre
+
+### Professor: Rodrigo Martins de Siqueira Barbosa
+
+---
+
+# Como utilizar este programa
---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*#######################################################################
+#                                                                       #
+#   Universidade Federal do Espírito Santo                              #
+#   INF09324 Programação Aplicada de Computadores 2021/1                #
+#   Primeiro trabalho                                                   #
+#                                                                       #
+#   Aluno: 2020205193 Felipe Pereira Umpierre                           #
+#   Professor: Rodrigo Martins de Siqueira Barbosa                      #
+#   Entrega: 01/09/2021                                                 #
+#                                                                       #
+#   Como utilizar: USO.pdf ou USO.md                                    #
+#   Sobre o desenvolvimento: DESENVOLVIMENTO.pdf ou DESENVOLVIMENTO.md  #
+#                                                                       #
+#######################################################################*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Primeiro trabalho de PAC" << std::endl;
+    return 0;
+}
